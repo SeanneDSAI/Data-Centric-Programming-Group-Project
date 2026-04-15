eda6ad80ab63f74868d4169ec108705686f02dab
Oluwadamise Adeniran <c24528163@mytudublin.ie>
2026-04-15 14:28:22 +0100

    adding comments

--- visualisations.py
+++ visualisations.py
@@ -2,8 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
 
+#loading the dataset
 df = pd.read_csv('flights.csv')
 
+#cleaning the 'arr_delay' column
 df = df.dropna(subset=[ 'arr_delay'])
 
+#Visualisations 
+
+#Average delay per carrier
 plt.figure(figsize=(10, 5))
