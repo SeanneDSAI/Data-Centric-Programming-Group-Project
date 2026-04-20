7f196033d9458f37f579681c844aad9de37880bb
Oluwadamise Adeniran <c24528163@mytudublin.ie>
2026-04-20 16:20:51 +0100

    visualisations charts 1 and 2

--- visualisations.py
+++ visualisations.py
@@ -9,5 +9,26 @@ df = pd.read_csv('flights.csv')
 
 #Visualisations 
 
-#Average delay per carrier
+#First Visualisation: Average delay per carrier
 plt.figure(figsize=(10, 5))
+avg_delay_by_carrier = df.groupby('carrier')['dep_delay'].mean().sort_values()
+sns.barplot(x = avg_delay_by_carrier.index,y = avg_delay_by_carrier.values,palette = 'magma')
+plt.title('Average delay per carrier')
+plt.xlabel('Carrier')
+plt.ylabel('Mintes Late')
+plt.show()
+plt.savefig('Avg_Carrier_Delays.png')
+plt.close()
+
+#Second Visualisation: Delay distribution by airline
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=df, x = 'carrier', y = 'dep_delay',showfliers = False)
+plt.title('Delay distribution by airline')
+plt.ylim(-50, 400)
+plt.yticks(range(0, 401, 50))
+plt.xlabel('Carrier')
+plt.ylabel('Delay per carrier')
+plt.savefig('Carrier_Delay_Distribution.png')
+plt.close()
+
+#Third Visualisation: Delay by hour of day and day of week
