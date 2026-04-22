f2bd268f7768fd1f13b9dd371abf43be9e59b0e8
dillon329 <c24452624@mytudublin.ie>
2026-04-22 16:35:23 +0100

    Exported clean datasets to json and csv files in data/processed

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 
-df = pd.read_csv("flights.csv")
+
+df = pd.read_csv("data/Raw/flights.csv")
+
+csv_file_name = "data/Processed/flights.csv"
+json_file_name = 'data/Processed/flights.json'
 
 print(df.head())
 
@@ -12,3 +16,9 @@ df = df.dropna()
 #  even by getting rid of all the null values 97% of values still remain
 
 print(len(df)/og_len)
+
+
+
+df.to_csv(csv_file_name, index=False)
+
+df.to_json(json_file_name, orient='split', compression='infer', index=True)
